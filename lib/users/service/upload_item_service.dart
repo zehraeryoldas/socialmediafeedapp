254450cb5809ef2080ff9f3aaa4b6d8849ec6eb9
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-28 01:04:46 +0300

    dateFormat settings

--- lib/users/service/upload_item_service.dart
+++ lib/users/service/upload_item_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:socialmediafeedapp/users/model/upload_model.dart';
+import 'package:intl/intl.dart';
 
 class UploadItemService {
   final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
@@ -11,8 +12,15 @@ class UploadItemService {
       DocumentReference result = await FirebaseFirestore.instance
           .collection("items") // Firestore koleksiyonunun adını buraya ekleyin
           .add(uploadItem.toJson());
-
+      var result2 = result.id;
+      final now = DateTime.now();
+      final formattedDate = DateFormat('dd MMM HH:mm').format(now);
       // Başarı durumunda Firestore'dan dönen belgenin ID'sini döndürün
+      FirebaseFirestore.instance.collection("items").doc(result2).update({
+        'id': result2,
+        'productStatus': 1,
+        'time_stamp': formattedDate,
+      });
       return result.id;
     } catch (e) {
       // Hata durumunda hata mesajını döndürün
