c48e5fbce55d20846ca840394142b6e37c92363e
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-27 15:31:48 +0300

    WidgetsFlutterBinding.ensureInitialized();

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:socialmediafeedapp/firebase_options.dart';
 import 'package:socialmediafeedapp/users/fragments/dashboard_fragment.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
   runApp(const MainApp());
 }
 
