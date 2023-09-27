0f1de343507546be3934d7ad1682aa9b918b290c
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-27 14:34:17 +0300

    edits were made

--- lib/users/fragments/dashboard_fragment.dart
+++ lib/users/fragments/dashboard_fragment.dart
@@ -3,14 +3,21 @@ import 'package:get/get_rx/src/rx_types/rx_types.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:socialmediafeedapp/users/fragments/home_fragment.dart';
 import 'package:socialmediafeedapp/users/fragments/upload_item.dart';
+import 'package:get/get.dart';
 
-class DashboardOfFragment extends StatelessWidget {
-  DashboardOfFragment({super.key});
+class DashboardOfFragment extends StatefulWidget {
+  const DashboardOfFragment({super.key});
 
+  @override
+  State<DashboardOfFragment> createState() => _DashboardOfFragmentState();
+}
+
+class _DashboardOfFragmentState extends State<DashboardOfFragment> {
   final List<Widget> _fragmentScreen = [
     const HomeFragmentScreen(),
     const UploadItemFragmentScreen()
   ];
+
   final List _navigationButtonProperties = [
     {
       "active_icon": Icons.home,
@@ -24,33 +31,29 @@ class DashboardOfFragment extends StatelessWidget {
     }
   ];
 
-  final RxInt _indexNumber = 0.obs;
+  int _indexNumber = 0;
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder(
-      builder: (controller) {
-        return Scaffold(
-          body: SafeArea(child: Obx(() => _fragmentScreen[_indexNumber.value])),
-          bottomNavigationBar: Obx(() => BottomNavigationBar(
-              currentIndex: _indexNumber.value,
-              onTap: (value) {
-                _indexNumber.value = value;
-              },
-              showSelectedLabels: true,
-              showUnselectedLabels: true,
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Colors.white24,
-              items: List.generate(2, (index) {
-                var navBtnProperty = _navigationButtonProperties[index];
-                return BottomNavigationBarItem(
-                    backgroundColor: Colors.black,
-                    icon: Icon(navBtnProperty["non_active_icon"]),
-                    activeIcon: Icon(navBtnProperty["active_icon"]),
-                    label: navBtnProperty["label"]);
-              }))),
-        );
-      },
-    );
+    return Scaffold(
+        body: _fragmentScreen[_indexNumber],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _indexNumber,
+            onTap: (value) {
+              setState(() {
+                _indexNumber = value;
+              });
+            },
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedItemColor: Colors.red,
+            unselectedItemColor: Colors.red.shade200,
+            items: List.generate(2, (index) {
+              var navBtnProperty = _navigationButtonProperties[index];
+              return BottomNavigationBarItem(
+                  icon: Icon(navBtnProperty["non_active_icon"]),
+                  activeIcon: Icon(navBtnProperty["active_icon"]),
+                  label: navBtnProperty["label"]);
+            })));
   }
 }
