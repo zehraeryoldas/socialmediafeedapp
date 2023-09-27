2e0cc8b6829ec6a073c3e877fd3507fe12b75246
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-27 15:32:02 +0300

    design created

--- lib/users/fragments/upload_item.dart
+++ lib/users/fragments/upload_item.dart
@@ -9,10 +9,122 @@ class UploadItemFragmentScreen extends StatefulWidget {
 }
 
 class _UploadItemFragmentScreenState extends State<UploadItemFragmentScreen> {
+  var formKey = GlobalKey<FormState>();
+  //tarih eklenir
+  final TextEditingController _titleText = TextEditingController();
+  final TextEditingController _bodytext = TextEditingController();
+  //resim alınacak
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient:
+                  LinearGradient(colors: [Colors.black54, Colors.deepPurple])),
+        ),
+        automaticallyImplyLeading: false,
+        title: const Text("Upload Form"),
+        centerTitle: true,
+      ),
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 8,
+                    color: Colors.black26,
+                    offset: Offset(0, -3),
+                  )
+                ]),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 30, 10, 8),
+              child: Column(
+                children: [
+                  Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          _myTextFormField(_titleText,
+                              "please write text title", "Text title"),
+                          const SizedBox(
+                            height: 18,
+                          ),
+                          _myTextFormField(_bodytext, "please write body title",
+                              "Body title"),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Material(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.circular(20),
+                              child: InkWell(
+                                onTap: () {},
+                                child: const Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 10,
+                                    horizontal: 28,
+                                  ),
+                                  child: Text(
+                                    "Upload new",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ))
+                ],
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+
+  TextFormField _myTextFormField(
+      TextEditingController controller, String metin, String hintext) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Colors.white60,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Colors.white60,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Colors.red,
+          ),
+        ),
+        disabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(
+            color: Colors.white60,
+          ),
+        ),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 6,
+        ),
+        fillColor: Colors.white,
+        filled: true,
+        hintText: hintext,
+      ),
+      controller: controller,
+      validator: (value) => value == "" ? metin : null,
     );
   }
 }
