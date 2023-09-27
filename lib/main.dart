49cdcfcaac06d30e08cd30dc6dcc685bda870270
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-27 13:31:39 +0300

    route determined

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 import 'package:socialmediafeedapp/users/fragments/dashboard_fragment.dart';
 
 void main() {
@@ -10,7 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: DashboardOfFragment(),
       debugShowCheckedModeBanner: false,
     );
