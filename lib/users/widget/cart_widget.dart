27492b1e47514d94971c4df999373ae225d5b7dc
Zehra ERYOLDAŞ <eryoldas577@gmail.com>
2023-09-29 23:34:45 +0300

    constuctor and route created

--- lib/users/widget/cart_widget.dart
+++ lib/users/widget/cart_widget.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:socialmediafeedapp/users/items/item_detail_screen.dart';
+import 'package:socialmediafeedapp/users/model/upload_model.dart';
 
 class CartWidget extends StatelessWidget {
   CartWidget({super.key});
@@ -24,52 +26,73 @@ class CartWidget extends StatelessWidget {
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
               DocumentSnapshot data = snapshot.data!.docs[index];
+
+              UploadItem uploadItem = UploadItem(
+                titleText: data['title_text'].toString(),
+                bodyText: data['body_text'].toString(),
+                images: data['images'].toString(),
+              );
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height * 0.35,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Colors.deepPurple,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ItemDetailsScreen(
+                            uploadItem: uploadItem,
+                            timeStamp: data['time_stamp'].toString(),
+                          ),
+                        ));
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.33,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              offset: Offset(0, 3),
+                              blurRadius: 6,
+                              color: Colors.deepPurpleAccent)
+                        ]),
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          data['images'].toString(),
+                          width: MediaQuery.of(context).size.width,
+                          height: 180,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return const Placeholder();
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ListTile(
+                          title: Text(
+                            data['title_text'],
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            data['body_text'],
+                            maxLines: 3,
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          trailing: Text(data['time_stamp'].toString()),
+                        ),
+                      ],
                     ),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.network(
-                        data['images'].toString(),
-                        width: MediaQuery.of(context).size.width,
-                        height: 180,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) {
-                          return const Placeholder();
-                        },
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      ListTile(
-                        title: Text(
-                          data['title_text'],
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          data['body_text'],
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        trailing: Text(data['time_stamp'].toString()),
-                      ),
-                    ],
                   ),
                 ),
               );
